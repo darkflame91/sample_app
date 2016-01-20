0570e8cd1ee7e6d452b47c8f77fc81120b6054df
darkflame91 <darkflame91@gmail.com>
2016-01-20 17:15:23 +0000

    Add full title helper

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ group :development, :test do
   gem 'sqlite3'
 end
 
+group :test do
+  gem 'minitest-reporters'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Dominic"
+    assert_select "title", "Dominic"
   end
 
   test "should get help" do
